af7de0f0337aaea6b52251f2a99672d36692e1af
iseki <admin@iseki.space>
2022-04-26 23:44:04 +0800

    init

---
+++ oneshot.go
@@ -0,0 +1,78 @@
+package oneshot
+
+import (
+	"context"
+	"sync"
+)
+
+type Source interface {
+	Wait(ctx context.Context, keys ...string) (timeout bool)
+}
+
+type Subject interface {
+	Emit(key string)
+}
+
+func New() (source Source, subject Subject) {
+	s := &_S{
+		L: &sync.Mutex{},
+		M: map[string]map[chan struct{}]struct{}{},
+	}
+	return s, s
+}
+
+type _S struct {
+	L sync.Locker
+	M map[string]map[chan struct{}]struct{}
+}
+
+type _Sub *_S
+
+func (this *_S) Emit(key string) {
+	var s []chan struct{}
+	this.L.Lock()
+	for it := range this.M[key] {
+		s = append(s, it)
+	}
+	this.L.Unlock()
+	for _, it := range s {
+		close(it)
+	}
+}
+func (this *_S) Wait(ctx context.Context, keys ...string) (timeout bool) {
+	if len(keys) == 0 {
+		return
+	}
+	if ctx == nil {
+		ctx = context.TODO()
+	}
+	ch := make(chan struct{})
+	this.L.Lock()
+	for _, it := range keys {
+		if this.M[it] == nil {
+			this.M[it] = map[chan struct{}]struct{}{}
+		}
+		this.M[it][ch] = struct{}{}
+	}
+	this.L.Unlock()
+
+	select {
+	case <-ctx.Done():
+		timeout = false
+	case <-ch:
+		timeout = true
+	}
+
+	this.L.Lock()
+	for _, it := range keys {
+		if this.M[it] == nil {
+			continue
+		}
+		delete(this.M[it], ch)
+		if len(this.M[it]) == 0 {
+			delete(this.M, it)
+		}
+	}
+	this.L.Unlock()
+	return
+}
---
+++ oneshot_test.go
@@ -0,0 +1,35 @@
+package oneshot
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestOneShot(t *testing.T) {
+	source, subject := New()
+	wg := sync.WaitGroup{}
+	go func() {
+		time.Sleep(time.Second * 1)
+		subject.Emit("foo")
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		_t := time.Now()
+		assert.True(t, source.Wait(context.TODO(), "foo", "bar"))
+		assert.True(t, time.Now().Sub(_t).Milliseconds() >= 1000, "duang~~~")
+		fmt.Println("=========", time.Now().Sub(_t).Microseconds())
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		timeout, cancelFunc := context.WithTimeout(context.TODO(), time.Millisecond*50)
+		defer cancelFunc()
+		assert.False(t, source.Wait(timeout, "bar"), "duang22")
+	}()
+	wg.Wait()
+}
